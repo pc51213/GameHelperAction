23eebe9ddb5d30ee30a3c7c1f514ab6299fac9dc
pc51213 <67721407+pc51213@users.noreply.github.com>
2026-04-12 02:22:21 +0800

    Update SmobaEntry.py

--- src/smoba/SmobaEntry.py
+++ src/smoba/SmobaEntry.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 from src.smoba.http import HttpApi
-from src.util import Util, PushPlus
+from src.util import Util, ServerChan
 
 send_content = ""
 
@@ -179,4 +179,4 @@ def entry():
             send_content += ">环境变量未配置\n"
         send_content += "#####################################\n"
         print(send_content)
-        PushPlus.send("王者营地-51213", send_content)
+        ServerChan.send("王者营地-51213", send_content)
